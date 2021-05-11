611c5450d4246bbfba55651f91f45956405b7932
HardCoder19 <manavmishra021999@gmail.com>
2021-05-11 23:50:43 +0530

    Initial commit

---
+++ example/lib/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String appName = 'TAP APP';
+  static const String logoTag = 'near.huscarl.loginsample.logo';
+  static const String titleTag = 'near.huscarl.loginsample.title';
+}
